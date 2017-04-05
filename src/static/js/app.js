86fed9c71bd22e274dafbf2d027f2b4c88f4bf21
Olivia Cheng <olivia@heyitsolivia.com>
2017-04-05 00:15:40 -0400

    Add form styles and form validation

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import domready from 'domready'
+import VeeValidate from 'vee-validate'
 
 import { householdData } from './data'
 
 
 Vue.use(VueRouter)
 Vue.use(Vuex)
+Vue.use(VeeValidate)
 
 const store = new Vuex.Store({
   state: {
@@ -22,8 +24,8 @@ const store = new Vuex.Store({
 
 const heroTemplate = `
   <section class="hero">
-      <div class="row">
-          <div class="small-12 columns">
+      <div class="row align-center">
+          <div class="small-11 columns">
               <h1 class="heading heading--large">{{ title }}</h1>
               <p class="heading heading--small">{{ description }}</p>
           </div>
@@ -33,8 +35,8 @@ const heroTemplate = `
 
 const summaryTemplate = `
 <section class="section">
-    <div class="row">
-        <div class="small-12 columns">
+    <div class="row align-center">
+        <div class="small-11 columns">
             <router-link tag="button" :to="{ name: 'addMember'}" class="button float-right">
                 Add new member
                 <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="22" height="16" viewBox="0 0 22 16">
@@ -42,18 +44,21 @@ const summaryTemplate = `
                   <path fill="#ffffff" d="M5 6V3H3v3H0v2h3v3h2V8h3V6M14 10c-2.7 0-8 1.3-8 4v2h16v-2c0-2.7-5.3-4-8-4z"/>
                 </svg>
             </router-link>
-
+        </div>
+    </div>
+    <div class="row align-center">
+        <div class="small-11 columns">
             <table class="table">
                 <thead>
                     <tr>
-                        <th scope="col">Full Name</th>
+                        <th class="primary-cell" scope="col">Name</th>
                         <th scope="col">Description</th>
                         <th scope="col">Favorite Fruit</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr v-for="member in household">
-                        <td>{{ member.fullName }}</td>
+                        <td class="primary-cell">{{ member.fullName }}</td>
                         <td>{{ member.description }}</td>
                         <td>{{ member.favoriteFruit }}</td>
                     </tr>
@@ -66,29 +71,50 @@ const summaryTemplate = `
 `
 
 const formTemplate = `
-<form>
-    <div class="row">
-        <div class="medium-4 columns">
-            <label>Full Name
-                <input v-model="fullName" type="text" placeholder="Your full name">
-            </label>
-        </div>
-        <div class="medium-4 columns">
-            <label>Description
-                <input v-model="description" type="text" placeholder="Your role">
-            </label>
-        </div>
-        <div class="medium-4 columns">
-            <label>Favorite Fruit
-                <input v-model="favoriteFruit" type="text" placeholder="Apple">
-            </label>
+<form @submit.prevent="validateBeforeSubmit" class="form">
+    <div class="form__section">
+        <div class="row align-center">
+            <div class="small-11 medium-8 large-4 columns">
+                <label class="form__label">Full Name</label>
+                <small>The full name as inscribed upon legal documents.</small>
+            </div>
+            <div class="small-11 medium-8 large-6 columns">
+                <input v-model="fullName" v-validate="'required'" :class="{'input': true, 'is-invalid': errors.has('name') }" name="name" type="text" placeholder="First and last name">
+                <span v-show="errors.has('name')" class="error is-invalid">{{ errors.first('name') }}</span>
+            </div>
         </div>
     </div>
-    <div class="row">
-        <div class="medium-12 columns">
-            <div class="float-right">
-                <button v-on:click="cancel" class="button">Cancel</button>
-                <button v-on:click="submit" class="button">Add member</button>
+    <div class="form__section">
+        <div class="row align-center">
+            <div class="small-11 medium-8 large-4 columns">
+                <label class="form__label">Description</label>
+                <small>The role that this person plays in the household.</small>
+            </div>
+            <div class="small-11 medium-8 large-6 columns">
+                <input v-model="description" v-validate="'required'" :class="{'input': true, 'is-invalid': errors.has('description') }" name="description" type="text" placeholder="Role in household">
+                <span v-show="errors.has('description')" class="error is-invalid">{{ errors.first('description') }}</span>
+            </div>
+        </div>
+    </div>
+    <div class="form__section">
+        <div class="row align-center">
+            <div class="small-11 medium-8 large-4 columns">
+                <label class="form__label">Favorite Fruit</label>
+                <small>The fruit most preferred above all others.</small>
+            </div>
+            <div class="small-11 medium-8 large-6 columns">
+                <input v-model="favoriteFruit" v-validate="'required'" :class="{'input': true, 'is-invalid': errors.has('fruit') }" name="fruit" type="text" placeholder="i.e. apple">
+                <span v-show="errors.has('fruit')" class="error is-invalid">{{ errors.first('fruit') }}</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="row align-center">
+            <div class="small-11 medium-8 large-10 columns">
+                <div class="float-right">
+                    <button type="button" v-on:click="onCancel" class="button button--ghost">Cancel</button>
+                    <button type="submit" class="button">Add member</button>
+                </div>
             </div>
         </div>
     </div>
@@ -137,17 +163,25 @@ const Form = Vue.component('form-component', {
       this.description = '';
       this.favoriteFruit = '';
     },
-    submit() {
-      const member = {}
-      member.fullName = this.cleanFullName
-      member.description = this.cleanDescription
-      member.favoriteFruit = this.cleanFavoriteFruit
-
-      store.commit('addMember', member)
-      this.resetForm()
-      router.push({ name: 'household' })
+    validateBeforeSubmit() {
+      this.$validator.validateAll().then(() => {
+        this.onSubmit()
+        console.log('validated')
+      }).catch(() => {
+        // console.log('There were errors in the form.')
+      });
     },
-    cancel() {
+    onSubmit() {
+        const member = {}
+        member.fullName = this.cleanFullName
+        member.description = this.cleanDescription
+        member.favoriteFruit = this.cleanFavoriteFruit
+
+        store.commit('addMember', member)
+        this.resetForm()
+        router.push({ name: 'household' })
+    },
+    onCancel() {
       router.push({ name: 'household' })
     }
   }
